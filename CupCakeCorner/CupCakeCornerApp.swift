e60e1db5d0945b711f3c1b61ef3f182ba8424fdc
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-21 05:02:02 -0300

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Bruno Oliveira on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
